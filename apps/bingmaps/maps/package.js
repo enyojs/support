93e4351b965926128b359c4ced0ffeec05827f97
Ben Combee <ben.combee@lge.com>
2013-05-10 15:47:23 -0500

    Add BingMaps sample app to support repo
    Enyo-DCO-1.1-Signed-Off-By: Ben Combee (ben.combee@lge.com)

---
+++ apps/bingmaps/maps/package.js
@@ -0,0 +1,4 @@
+enyo.depends(
+	"BingMap.css",
+	"BingMap.js"
+);
---
+++ apps/bingmaps/package.js
@@ -0,0 +1,15 @@
+﻿enyo.depends(
+	"$lib/layout/fittable",
+	"$lib/layout/slideable",
+	"$lib/onyx",
+	"maps",
+	"source/mockdata.js",
+	"source/CurrentLocation.js",
+	"source/SearchPlaces.js",
+	"source/LabeledItem.js",
+	"source/BookmarkList.js",
+	"source/Pullout.js",
+	"source/Infobox.js",
+	"source/MapsApp.css",
+	"source/MapsApp.js"
+);
